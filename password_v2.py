642508783b5207083ca45c46b994c667d2cde4f2
roger tu <zongyi0520@gmail.com>
2021-06-20 19:34:38 +0800

    修正第0次機會問題

--- password_v2.py
+++ password_v2.py
@@ -1,16 +1,17 @@
 
 password = 'a123456'
 count = 3 
-while True:
+while count > 0:
+    count -=1
     pwd = input("請輸入密碼: ")
     if pwd == password:
         print('登入成功!!!')
         break
     else:
-        count -=1
-        if count != 0:
-            print('密碼錯誤!  還有', count, '次機會')
-        elif count == 0:
-            print('你不能登入了!!!')    
-            break
+        print('密碼錯誤')
+        if count > 0:
+            print('還有', count, '次機會')
+        else:
+            print('你不能在登入了')
+
         
